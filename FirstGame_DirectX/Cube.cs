9b4218dc79f497f0b413dfd2c6707788a95e3d31
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2021-02-04 16:49:34 +0100

    Add Cube Class. add Abstract Lesson Class. Finalize Homework Assigment

---
+++ FirstGame_DirectX/Cube.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+namespace FirstGame_DirectX
+{
+	public enum CubeColorMode
+	{
+		Preset = 0,
+		Disco = 1,
+		Headache = 2,
+		ColorIncrease = 3
+	}
+
+	class Cube
+	{
+		private CubeColorMode colorMode;
+		private Color colorVert1;
+		private Color colorVert2;
+		private Color colorVert3;
+		private Color colorVert4;
+
+		public VertexPositionColor[] Vertices { get; private set; }
+		public int[] Indices { get; private set; }
+		public int Primitives { get { return Indices.Length / 3; } private set { } }
+
+		public Vector3 Position { get; set; }
+		public Color ColorVert1 { get => colorVert1; set => colorVert1 = value; }
+		public Color ColorVert2 { get => colorVert2; set => colorVert2 = value; }
+		public Color ColorVert3 { get => colorVert3; set => colorVert3 = value; }
+		public Color ColorVert4 { get => colorVert4; set => colorVert4 = value; }
+		public CubeColorMode ColorMode { get => colorMode; set => colorMode = value; }
+
+		public void Draw(GraphicsDevice device, GameTime gameTime)
+		{
+			switch(colorMode)
+			{
+				case CubeColorMode.Preset:
+					break;
+				case CubeColorMode.Disco:
+					DiscoMode();
+					break;
+				case CubeColorMode.Headache:
+					HeadacheMode();
+					break;
+				case CubeColorMode.ColorIncrease:
+					ColorIncrease(gameTime);
+					break;
+				default:
+					break;
+			}
+
+			VertexPositionColor[] vertices = {
+			//+X
+			new VertexPositionColor(new Vector3(-Position.X , Position.Y, Position.Z), colorVert1),
+			new VertexPositionColor(new Vector3(Position.X , -Position.Y, Position.Z), colorVert2),
+			new VertexPositionColor(new Vector3(-Position.X , -Position.Y, Position.Z), colorVert3),
+			new VertexPositionColor(new Vector3(Position.X , Position.Y, Position.Z), colorVert4),
+			
+			//-X
+			new VertexPositionColor(new Vector3(-Position.X , Position.Y, -Position.Z), colorVert1),
+			new VertexPositionColor(new Vector3(Position.X , -Position.Y, -Position.Z), colorVert2),
+			new VertexPositionColor(new Vector3(-Position.X , -Position.Y, -Position.Z), colorVert3),
+			new VertexPositionColor(new Vector3(Position.X , Position.Y, -Position.Z), colorVert4),
+
+			//+Z
+			new VertexPositionColor(new Vector3(Position.X , -Position.Y, Position.Z), colorVert2),
+			new VertexPositionColor(new Vector3(Position.X , Position.Y, -Position.Z), colorVert4),
+			new VertexPositionColor(new Vector3(Position.X , -Position.Y, -Position.Z), colorVert2),
+			new VertexPositionColor(new Vector3(Position.X , Position.Y, Position.Z), colorVert4),
+
+			//-Z
+			new VertexPositionColor(new Vector3(-Position.X , -Position.Y, Position.Z), colorVert3),
+			new VertexPositionColor(new Vector3(-Position.X , Position.Y, -Position.Z), colorVert1),
+			new VertexPositionColor(new Vector3(-Position.X , -Position.Y, -Position.Z), colorVert3),
+			new VertexPositionColor(new Vector3(-Position.X , Position.Y, Position.Z), colorVert1),
+
+			//+Y
+			new VertexPositionColor(new Vector3(-Position.X , -Position.Y, Position.Z), colorVert3),
+			new VertexPositionColor(new Vector3(Position.X , -Position.Y, -Position.Z), colorVert2),
+			new VertexPositionColor(new Vector3(-Position.X , -Position.Y, -Position.Z), colorVert3),
+			new VertexPositionColor(new Vector3(Position.X , -Position.Y, Position.Z), colorVert2),
+
+			//-Y
+			new VertexPositionColor(new Vector3(-Position.X , Position.Y, -Position.Z), colorVert1),
+			new VertexPositionColor(new Vector3(Position.X , Position.Y, Position.Z), colorVert4),
+			new VertexPositionColor(new Vector3(-Position.X , Position.Y, Position.Z), colorVert1),
+			new VertexPositionColor(new Vector3(Position.X , Position.Y, -Position.Z), colorVert4),
+			};
+
+			int[] indices = {
+			//FRONT
+			//triangle 1
+			0, 1, 2,
+			//triangle 2
+			0, 3, 1,
+			//BACK
+			//triangle 1
+			4, 6, 5,
+			//triangle 2
+			4, 5, 7,
+
+			//RIGHT
+			//triangle 1
+			8, 9, 10,
+			//triangle 2
+			8, 11, 9,
+			//LEFT
+			//triangle 1
+			12, 14, 13,
+			//triangle 2
+			12, 13, 15,
+
+			//DOWN
+			//triangle 1
+			16, 17, 18,
+			//triangle 2
+			16, 19, 17,
+			//UP
+			//triangle 1
+			20, 21, 22,
+			//triangle 2
+			20, 23, 21
+			};
+
+			Vertices = vertices;
+			Indices = indices;
+
+			device.DrawUserIndexedPrimitives(PrimitiveType.TriangleList, Vertices, 0, Vertices.Length, Indices, 0, Primitives);
+		}
+
+		public void DiscoMode()
+		{
+			Random rand = new Random(System.DateTime.Now.Millisecond);
+			colorVert1 = new Color(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255));
+			rand = new Random(System.DateTime.Now.Millisecond);
+			colorVert2 = new Color(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255));
+			rand = new Random(System.DateTime.Now.Millisecond);
+			colorVert3 = new Color(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255));
+			rand = new Random(System.DateTime.Now.Millisecond);
+			colorVert4 = new Color(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255));
+		}
+
+		public void HeadacheMode()
+		{
+			Random rand = new Random(System.DateTime.Now.Millisecond);
+			colorVert1 = new Color(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255));
+			colorVert2 = new Color(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255));
+			colorVert3 = new Color(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255));
+			colorVert4 = new Color(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255));
+		}
+
+		//TODO:
+		// Actually fix this thing. As of right now it just fades the whole cube to white...
+		public void ColorIncrease(GameTime gameTime)
+		{
+			Random rand = new Random(System.DateTime.Now.Millisecond);
+			colorVert1 = Color.Lerp(ColorVert1, new Color(rand.Next(200, 255), rand.Next(200, 255), rand.Next(200, 255)), (float)gameTime.ElapsedGameTime.TotalSeconds);
+			rand = new Random(System.DateTime.Now.Millisecond);
+			colorVert2 = Color.Lerp(ColorVert2, new Color(rand.Next(200, 255), rand.Next(200, 255), rand.Next(200, 255)), (float)gameTime.ElapsedGameTime.TotalSeconds);
+			rand = new Random(System.DateTime.Now.Millisecond);
+			colorVert3 = Color.Lerp(ColorVert3, new Color(rand.Next(200, 255), rand.Next(200, 255), rand.Next(200, 255)), (float)gameTime.ElapsedGameTime.TotalSeconds);
+			rand = new Random(System.DateTime.Now.Millisecond);
+			colorVert4 = Color.Lerp(ColorVert4, new Color(rand.Next(200, 255), rand.Next(200, 255), rand.Next(200, 255)), (float)gameTime.ElapsedGameTime.TotalSeconds);
+
+			Console.WriteLine(colorVert1);
+		}
+	}
+}
--- FirstGame_DirectX/Game1.cs
+++ FirstGame_DirectX/Game1.cs
@@ -8,96 +8,25 @@ namespace FirstGame_DirectX
 	{
 		private GraphicsDeviceManager _graphics;
 		private SpriteBatch _spriteBatch;
-
-		VertexPositionColor[] vertices = {
-			//+X
-			new VertexPositionColor( new Vector3( -1, 1, 1 ), Color.Red ),
-			new VertexPositionColor( new Vector3( 1, -1, 1 ), Color.Green ),
-			new VertexPositionColor( new Vector3( -1, -1, 1 ), Color.Blue ),
-			new VertexPositionColor( new Vector3( 1, 1, 1 ), Color.Yellow ),
-			
-			//-X
-			new VertexPositionColor( new Vector3( -1, 1, -1 ), Color.Red ),
-			new VertexPositionColor( new Vector3( 1, -1, -1 ), Color.Green ),
-			new VertexPositionColor( new Vector3( -1, -1, -1 ), Color.Blue ),
-			new VertexPositionColor( new Vector3( 1, 1, -1 ), Color.Yellow ),
-
-			//+Z
-			new VertexPositionColor( new Vector3( 1, -1, 1 ), Color.Green ),
-			new VertexPositionColor( new Vector3( 1, 1, -1 ), Color.Yellow ),
-			new VertexPositionColor( new Vector3( 1, -1, -1 ), Color.Green ),
-			new VertexPositionColor( new Vector3( 1, 1, 1 ), Color.Yellow ),
-
-			//-Z
-			new VertexPositionColor( new Vector3( -1, -1, 1 ), Color.Blue ),
-			new VertexPositionColor( new Vector3( -1, 1, -1 ), Color.Red ),
-			new VertexPositionColor( new Vector3( -1, -1, -1 ), Color.Blue ),
-			new VertexPositionColor( new Vector3( -1, 1, 1 ), Color.Red ),
-
-			//+Y
-			new VertexPositionColor( new Vector3( -1, -1, 1 ), Color.Blue ),
-			new VertexPositionColor( new Vector3( 1, -1, -1 ), Color.Green ),
-			new VertexPositionColor( new Vector3( -1, -1, -1 ), Color.Blue ),
-			new VertexPositionColor( new Vector3( 1, -1, 1 ), Color.Green ),
-
-			//-Y
-			new VertexPositionColor( new Vector3( -1, 1, -1 ), Color.Red ),
-			new VertexPositionColor( new Vector3( 1, 1, 1 ), Color.Yellow ),
-			new VertexPositionColor( new Vector3( -1, 1, 1 ), Color.Red ),
-			new VertexPositionColor( new Vector3( 1, 1, -1 ), Color.Yellow ),
-		};
-
-		int[] indices = {
-			//FRONT
-			//triangle 1
-			0, 1, 2,
-			//triangle 2
-			0, 3, 1,
-			//BACK
-			//triangle 1
-			4, 6, 5,
-			//triangle 2
-			4, 5, 7,
-
-			//RIGHT
-			//triangle 1
-			8, 9, 10,
-			//triangle 2
-			8, 11, 9,
-			//LEFT
-			//triangle 1
-			12, 14, 13,
-			//triangle 2
-			12, 13, 15,
-
-			//DOWN
-			//triangle 1
-			16, 17, 18,
-			//triangle 2
-			16, 19, 17,
-			//UP
-			//triangle 1
-			20, 21, 22,
-			//triangle 2
-			20, 23, 21
-		};
-
-		BasicEffect effect;
+		private Lesson currentLesson;
 
 		public Game1()
 		{
 			_graphics = new GraphicsDeviceManager(this);
 
-			_graphics.PreferredBackBufferWidth = 500;
-			_graphics.PreferredBackBufferHeight = 500;
-
 			Content.RootDirectory = "Content";
 			IsMouseVisible = true;
+
+			currentLesson = new Lesson1();
 		}
 
 		protected override void Initialize()
 		{
-			// TODO: Add your initialization logic here
+			currentLesson.Initialize();
+
+			_graphics.PreferredBackBufferWidth = 512;
+			_graphics.PreferredBackBufferHeight = 512;
+			_graphics.ApplyChanges();
 
 			base.Initialize();
 		}
@@ -106,34 +35,23 @@ namespace FirstGame_DirectX
 		{
 			_spriteBatch = new SpriteBatch(GraphicsDevice);
 
-			effect = new BasicEffect(GraphicsDevice);
+			currentLesson.LoadContent(Content, _graphics, _spriteBatch);
 		}
 
 		protected override void Update(GameTime gameTime)
 		{
-			if(GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape)) Exit();
+			if(GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
 
-			// TODO: Add your update logic here
-
+			currentLesson.Update(gameTime);
 			base.Update(gameTime);
 		}
 
 		protected override void Draw(GameTime gameTime)
 		{
-			GraphicsDevice.Clear(Color.Black);
+			GraphicsDevice.Clear(Color.CornflowerBlue);
 
-			effect.World =
-			Matrix.Identity * Matrix.CreateRotationY(-(float)gameTime.TotalGameTime.TotalSeconds) *
-			Matrix.Identity * Matrix.CreateRotationX((float)gameTime.TotalGameTime.TotalSeconds) *
-			Matrix.Identity * Matrix.CreateRotationZ(-(float)gameTime.TotalGameTime.TotalSeconds);
-			effect.View = Matrix.CreateLookAt(-Vector3.Forward * 5, Vector3.Zero, Vector3.Up);
-			effect.Projection = Matrix.CreatePerspectiveFieldOfView((MathHelper.Pi / 180f) * 65f, GraphicsDevice.Viewport.AspectRatio, 0.1f, 100f);
-
-			effect.VertexColorEnabled = true;
-			effect.CurrentTechnique.Passes[0].Apply();
-
-			GraphicsDevice.DrawUserIndexedPrimitives(PrimitiveType.TriangleList, vertices, 0, vertices.Length, indices, 0, indices.Length / 3);
-
+			currentLesson.Draw(gameTime, _graphics, _spriteBatch);
 			base.Draw(gameTime);
 		}
 	}
